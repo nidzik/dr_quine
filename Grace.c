a70b39343cd01d2b442a865fc9b94137170ffd3e
nidzik <nicolas.idzik@yahoo.fr>
2017-01-30 22:35:25 +0100

    fix Grace.c

--- Grace.c
+++ Grace.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
+//comment
 #define CWR O_CREAT|O_WRONLY
 #define NW "Grace_kid.c"
-#define M int main(){int fd=open(NW,CWR,0644);if(fd==-1)return(-1);char*i="#include <fcntl.h>%c#include <stdio.h>%c#define CWR O_CREAT|O_WRONLY%c#define NW %cGrace_kid.c%c%c#define M int main(){int fd=open(NW,CWR,0644);if(fd==-1)return(-1);char*i=%c%s%c;dprintf(fd,i,10,10,10,34,34,10,34,i,34,10,10);}%cM%c";dprintf(fd,i,10,10,10,34,34,10,34,i,34,10,10);}
+#define M int main(){int fd=open(NW,CWR,0644);if(fd==-1)return(-1);char*i="#include <fcntl.h>%c#include <stdio.h>%c//comment%c#define CWR O_CREAT|O_WRONLY%c#define NW %cGrace_kid.c%c%c#define M int main(){int fd=open(NW,CWR,0644);if(fd==-1)return(-1);char*i=%c%s%c;dprintf(fd,i,10,10,10,10,34,34,10,34,i,34,10,10);}%cM%c";dprintf(fd,i,10,10,10,10,34,34,10,34,i,34,10,10);}
 M
